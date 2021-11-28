32681c31054cf600337b2fa4fb45d9e5bab150e4
cjdjczym <1732018999@qq.com>
2021-11-28 14:47:10 +0800

    [fix] 修复了个人界面默认头像的颜色问题

--- lib/auth/view/user/user_avatar_image.dart
+++ lib/auth/view/user/user_avatar_image.dart
@@ -3,18 +3,17 @@ import 'package:we_pei_yang_flutter/commons/preferences/common_prefs.dart';
 
 class UserAvatarImage extends StatelessWidget {
   final double size;
+  final Color iconColor;
 
-  UserAvatarImage({@required this.size});
+  UserAvatarImage(
+      {@required this.size,
+      this.iconColor = const Color.fromRGBO(98, 103, 124, 1)});
 
   @override
   Widget build(BuildContext context) {
     var avatar = CommonPreferences().avatar.value;
     return avatar == ''
-        ? Icon(
-            Icons.account_circle_rounded,
-            size: size,
-            color: Color.fromRGBO(98, 103, 124, 1),
-          )
+        ? Icon(Icons.account_circle_rounded, size: size, color: iconColor)
         : CircleAvatar(
             radius: size / 2,
             backgroundColor: Color.fromRGBO(98, 103, 124, 1),
