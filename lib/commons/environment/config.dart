742f650a50b3750af16491b8b590f76454917b39
what <906885696@qq.com>
2022-05-01 21:36:33 +0800

    改一下config中的版本

--- lib/commons/environment/config.dart
+++ lib/commons/environment/config.dart
@@ -31,13 +31,13 @@ class EnvConfig {
   /// 微北洋版本信息，请勿修改代码，这里的默认值由脚本生成
   static const VERSION = String.fromEnvironment(
     "VERSION",
-    defaultValue: "4.3.5",
+    defaultValue: "4.2.7",
   );
 
   /// 微北洋版本信息，请勿修改代码，这里的默认值由脚本生成
   static const VERSIONCODE = int.fromEnvironment(
     "VERSIONCODE",
-    defaultValue: 105,
+    defaultValue: 103,
   );
 
   /// 青年湖底域名 "https://www.zrzz.site:7013/" (DEFAULT) 或 "https://qnhd.twt.edu.cn/"
