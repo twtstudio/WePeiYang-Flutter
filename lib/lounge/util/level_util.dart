18dee537939933c15bbafd3aeb86980e63937650
ly-xxx <bsliyuxiao@126.com>
2022-08-27 01:44:04 +0800

    [fix] 0级改成灰色

--- lib/lounge/util/level_util.dart
+++ lib/lounge/util/level_util.dart
@@ -33,29 +33,21 @@ class LevelUtil extends StatelessWidget {
         gradient: LinearGradient(
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
-            colors: [
-              double.parse(level) >= 0
-                  ? colors[(double.parse(level) / 10).floor() % 10]
-                  : Color.fromRGBO(85, 0, 9, 1.0),
-              double.parse(level) >= 0
-                  ? double.parse(level) >= 50
-                      ? colors[(double.parse(level) / 10).floor() % 10]
-                          .withAlpha(190)
-                      : colors[(double.parse(level) / 10).floor() % 10]
-                  : Color.fromRGBO(0, 0, 0, 1.0),
-            ],
-            stops: [
-              0.5,
-              0.8
-            ]),
+            colors: double.parse(level) == 0
+                ? [Colors.black26, Colors.black26]
+                : [
+                    double.parse(level) >= 0
+                        ? colors[(double.parse(level) / 10).floor() % 10]
+                        : Color.fromRGBO(85, 0, 9, 1.0),
+                    double.parse(level) >= 0
+                        ? double.parse(level) >= 50
+                            ? colors[(double.parse(level) / 10).floor() % 10]
+                                .withAlpha(190)
+                            : colors[(double.parse(level) / 10).floor() % 10]
+                        : Color.fromRGBO(0, 0, 0, 1.0),
+                  ],
+            stops: [0.5, 0.8]),
         borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0, 4),
-            blurRadius: 10,
-            color: Colors.black.withOpacity(0.05),
-          ),
-        ],
       ),
       child: Center(
         child: Text(
