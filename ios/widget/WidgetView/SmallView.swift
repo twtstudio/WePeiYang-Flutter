7a57dc2abfd00f5db3ffd264a6df974b42b6819f
Zrzzzz <1160026659@qq.com>
2022-10-18 21:27:31 +0800

    [fea] 夜间模式

--- ios/widget/WidgetView/SmallView.swift
+++ ios/widget/WidgetView/SmallView.swift
@@ -27,16 +27,20 @@ struct SmallView: View {
     
     @State var courses: [WCourse] = []
     
+    private var colorProperty: ColorProperty {
+        ColorProperty(wTheme: theme, colorTheme: colorScheme)
+    }
+    
     private func ClassLine(course: WCourse) -> some View {
         return HStack {
             RoundedRectangle(cornerRadius: 3)
-                .fill(Color.wColor(.main, theme))
+                .fill(Color.wColor(.main, colorProperty))
                 .frame(width: 4, height: 28)
             VStack(alignment: .leading) {
                 HStack(alignment: .center, spacing: 2) {
                     Text(course.course.name)
                         .wfont(.pingfang, size: 11)
-                        .foregroundColor(.wColor(.title, theme))
+                        .foregroundColor(.wColor(.title, colorProperty))
                         .lineLimit(1)
                     Group {
                         if course.isDup {
@@ -52,11 +56,11 @@ struct SmallView: View {
                 // 自动缩小防止显示不全
                 HStack(spacing: 0) {
                     Text("\(course.arrange.location) ")
-                        .foregroundColor(.wColor(.body, theme))
+                        .foregroundColor(.wColor(.body, colorProperty))
                         .wfont(.sfpro, size: 11)
                     
                     Text("\(course.arrange.unitTimeString)")
-                        .foregroundColor(.wColor(.body, theme))
+                        .foregroundColor(.wColor(.body, colorProperty))
                         .wfont(.sfpro, size: 11)
                         .fixedSize(horizontal: true, vertical: false)
                 }
@@ -67,13 +71,13 @@ struct SmallView: View {
     private func TomorrowLogo() -> some View {
         Text("明天")
             .wfont(.pingfang, size: 10)
-            .foregroundColor(.wColor(.main, theme))
+            .foregroundColor(.wColor(.main, colorProperty))
     }
     
     private func MoreCourses(cnt: Int) -> some View {
         Text("今天还有 \(cnt) 个课程")
             .wfont(.pingfang, size: 10)
-            .foregroundColor(.wColor(.body, theme))
+            .foregroundColor(.wColor(.body, colorProperty))
     }
     
     private func DetailView() -> some View {
@@ -107,6 +111,23 @@ struct SmallView: View {
         }
     }
     
+    private func bgView(theme: WColorTheme, colorScheme: ColorScheme) -> some View {
+        Group {
+            if colorScheme == .dark {
+                Color.hex("#0A0A0A")
+            } else {
+                theme == .blue ?
+                AnyView(LinearGradient(colors: [
+                    .hex("#3586E2"),
+                    .hex("#3F8FE3"),
+                    .hex("#519FE4"),
+                    .hex("#70BAE7"),
+                ], startPoint: .topLeading, endPoint: .bottomTrailing))
+                : AnyView(Color.white)
+            }
+        }
+    }
+    
     
     var body: some View {
         Group {
@@ -117,14 +138,14 @@ struct SmallView: View {
                 VStack(alignment: .leading) {
                     HStack(alignment: .top) {
                         Text("\(weekday).")
-                            .foregroundColor(.wColor(.main, theme))
+                            .foregroundColor(.wColor(.main, colorProperty))
                             .wfont(.product, size: 36)
                             .fixedSize(horizontal: false, vertical: true)
                             .lineLimit(1)
                         
                         Spacer()
                         
-                        Image(theme == .white ? "peiyanglogo-white" : "peiyanglogo-blue")
+                        Image(theme == .white || colorScheme == .dark ? "peiyanglogo-white" : "peiyanglogo-blue")
                             .resizable()
                             .frame(width: 21, height: 10)
                             .padding(.top, 10)
@@ -134,7 +155,7 @@ struct SmallView: View {
                     if courses.isEmpty {
                         Text("这两天都没有课程啦，\n假期愉快！")
                             .wfont(.pingfang, size: 10)
-                            .foregroundColor(.wColor(.title, theme))
+                            .foregroundColor(.wColor(.title, colorProperty))
                     } else {
                         DetailView()
                     }
@@ -143,14 +164,7 @@ struct SmallView: View {
                     
                 }
                 .padding()
-                .background(theme == .blue ?
-                            AnyView(LinearGradient(colors: [
-                                .hex("#3586E2"),
-                                .hex("#3F8FE3"),
-                                .hex("#519FE4"),
-                                .hex("#70BAE7"),
-                            ], startPoint: .topLeading, endPoint: .bottomTrailing))
-                            : AnyView(Color.white))
+                .background(bgView(theme: theme, colorScheme: colorScheme))
                 .onAppear {
                     store.reloadData()
                     courses = WidgetCourseManager.getCourses(courseTable: courseTable)
