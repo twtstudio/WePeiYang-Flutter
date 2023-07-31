c61493066d423e1307c088ac2c9059371ffc4dab
xyy9233 <1768765226@qq.com>
2023-08-01 00:40:13 +0800

    修改一下ui

--- lib/feedback/view/lost_and_found_page/lost_and_found_detail_page.dart
+++ lib/feedback/view/lost_and_found_page/lost_and_found_detail_page.dart
@@ -85,14 +85,14 @@ class LostAndFoundDetailPage extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: LostAndFoundDetailAppBar(
         leading: Padding(
-          padding: EdgeInsets.only(top: 46, left: 30),
+          padding: EdgeInsets.only(top: 46.h, left: 16.w),
           child: Align(
-            alignment: Alignment.topRight,
+            alignment: Alignment.centerLeft,
             child: WButton(
               child: WpyPic(
-                'assets/svg_pics/laf_butt_icons/back_black.svg',
-                width: 20.w,
-                height: 20.w,
+                'assets/images/back.png',
+                width: 30.w,
+                height: 30.w,
               ),
               onPressed: () {
                 Navigator.pop(context);
@@ -102,23 +102,21 @@ class LostAndFoundDetailPage extends StatelessWidget {
           ),
         ),
         action: Padding(
-          padding: EdgeInsets.only(top: 46, right: 30),
+          padding: EdgeInsets.only(top: 46.h, right: 16.w),
           child: Align(
-            alignment: Alignment.topRight,
+            alignment: Alignment.centerRight,
             child: WButton(
               child: WpyPic(
                 'assets/images/more-horizontal.png',
                 width: 30.w,
                 height: 30.w,
               ),
-              onPressed: () {
-
-              },
+              onPressed: () {},
               // Other onPressed logic goes here
             ),
           ),
         ),
-        title: Text(''),
+        title: Text('   '),
       ),
       body: SafeArea(
         child: Column(
@@ -137,10 +135,10 @@ class LostAndFoundDetailPage extends StatelessWidget {
                           children: [
                             Image.asset(
                               'assets/images/school.png',
-                              width: 32,
-                              height: 32,
+                              width: 32.w,
+                              height: 32.h,
                             ),
-                            SizedBox(width: 13),
+                            SizedBox(width: 13.w),
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
@@ -151,7 +149,7 @@ class LostAndFoundDetailPage extends StatelessWidget {
                                     color: Colors.black,
                                   ),
                                 ),
-                                SizedBox(height: 6),
+                                SizedBox(height: 6.h),
                                 Column(
                                   children: [
                                     Text(
@@ -192,13 +190,13 @@ class LostAndFoundDetailPage extends StatelessWidget {
                         fontWeight: FontWeight.w600,
                       ),
                     ),
-                    SizedBox(height: 2),
+                    SizedBox(height: 5.h),
                     Image.asset(
                       'assets/images/schedule_empty.png',
                       width: 360,
                       height: 204,
                     ),
-                    SizedBox(height: 14),
+                    SizedBox(height: 14.h),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
@@ -224,7 +222,7 @@ class LostAndFoundDetailPage extends StatelessWidget {
                                   ),
                                 ),
                               ),
-                              SizedBox(width: 2),
+                              SizedBox(width: 2.w),
                               Text(
                                 '其他',
                                 style: TextStyle(
@@ -236,7 +234,7 @@ class LostAndFoundDetailPage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          '11445次浏览'+'       ',
+                          '11445次浏览' + '       ',
                           style: TextStyle(
                             fontSize: 9,
                             color: Color(0xFF909090),
@@ -244,30 +242,28 @@ class LostAndFoundDetailPage extends StatelessWidget {
                         )
                       ],
                     ),
-                    SizedBox(height: 28),
+                    SizedBox(height: 28.h),
                     Padding(
-                      padding: EdgeInsets.all(7), // Add 10-pixel padding around the Text widget
+                      padding: EdgeInsets.all(7),
                       child: Text(
                         '求好心人带回可爱柴柴求好心人带回可爱柴柴求好心人带回可爱柴柴求好心人带回可爱柴柴求好心人带回可爱柴柴求好心人带回可爱柴柴',
                         style: TextStyle(
                           fontSize: 15,
-                          height: 1.5,
-                          color: Color.fromRGBO(42, 42, 42, 1), // Set the text color here
+                          height: 1.5.h,
+                          color: Color.fromRGBO(42, 42, 42, 1),
                         ),
                       ),
                     ),
-                    SizedBox(height: 45),
+                    SizedBox(height: 45.h),
                     Padding(
-                      padding: EdgeInsets.only(left: 4),
+                      padding: EdgeInsets.only(left: 4.w),
                       child: Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text('丢失日期',
                               style:
-                              TextStyle(fontSize: 14, color: Colors.black)),
-                          SizedBox(
-                              width:
-                              15), // Add some horizontal spacing between the text and date
+                                  TextStyle(fontSize: 14, color: Colors.black)),
+                          SizedBox(width: 15.w),
                           Text('2023-03-31',
                               style: TextStyle(
                                   fontSize: 14,
@@ -276,10 +272,10 @@ class LostAndFoundDetailPage extends StatelessWidget {
                         ],
                       ),
                     ),
-                    SizedBox(height: 40),
+                    SizedBox(height: 40.h),
                     Padding(
-                      padding: EdgeInsets.only(left: 4),
-                      child: Column(
+                      padding: EdgeInsets.only(left: 4.w),
+                      child: Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
@@ -289,7 +285,7 @@ class LostAndFoundDetailPage extends StatelessWidget {
                               color: Colors.black,
                             ),
                           ),
-                          SizedBox(height: 15),
+                          SizedBox(width: 15.w),
                           Text(
                             '图书馆到诚八沿途',
                             style: TextStyle(
@@ -301,36 +297,44 @@ class LostAndFoundDetailPage extends StatelessWidget {
                         ],
                       ),
                     ),
-                    SizedBox(height: 85),
+                    SizedBox(height: 85.h),
                     Row(
                       children: [
-                        Container(
-                          width: 110,
-                          margin: EdgeInsets.only(left: 50),
-                          padding: EdgeInsets.symmetric(vertical: 10),
-                          decoration: BoxDecoration(
-                            color: Color(0xFF2C7EDF),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Text(
-                            '我找到了',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
-                            ),
-                          ),
+                        SizedBox(
+                          width: 10.w,
                         ),
-                        const SizedBox(width:30,),
                         Container(
-                          width: 110,
-                          height: 40,
-                          margin: EdgeInsets.only(left: 30),
+                            width: 110.w,
+                            height: 40.h,
+                            margin: EdgeInsets.only(left: 30.w),
+                            decoration: BoxDecoration(
+                              color: Color(0xFF2C7EDF),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    '我找到了',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ])),
+                        SizedBox(
+                          width: 30.w,
+                        ),
+                        Container(
+                          width: 110.w,
+                          height: 40.h,
+                          margin: EdgeInsets.only(left: 30.w),
                           decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(20),
                             border: Border.all(
                               color: Color(0xFF2C7EDF),
-                              width: 1,
+                              width: 1.w,
                             ),
                           ),
                           child: Row(
@@ -338,10 +342,10 @@ class LostAndFoundDetailPage extends StatelessWidget {
                             children: [
                               Image.asset(
                                 'assets/images/octicon_light-bulb-24.png',
-                                width: 24,
-                                height: 20,
+                                width: 24.w,
+                                height: 20.h,
                               ),
-                              SizedBox(width: 8),
+                              SizedBox(width: 8.w),
                               Text(
                                 '擦亮',
                                 style: TextStyle(
