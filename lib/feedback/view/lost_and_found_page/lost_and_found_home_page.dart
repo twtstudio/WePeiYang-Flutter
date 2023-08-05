4b519743ed660abf950bbf1473863bdfb8087884
MalogPeter <2557826346@qq.com>
2023-08-05 09:12:39 +0800

    [new] 封装详情页图片组件

--- lib/feedback/view/lost_and_found_page/lost_and_found_home_page.dart
+++ lib/feedback/view/lost_and_found_page/lost_and_found_home_page.dart
@@ -7,18 +7,14 @@ import 'package:we_pei_yang_flutter/feedback/util/color_util.dart';
 import 'package:we_pei_yang_flutter/feedback/view/lost_and_found_page/lost_and_found_post_page.dart';
 import 'package:we_pei_yang_flutter/feedback/view/lost_and_found_page/lost_and_found_sub_page.dart';
 
-import '../../feedback_router.dart';
-
 class LostAndFoundHomePage extends StatefulWidget {
-
   LostAndFoundHomePage({Key? key}) : super(key: key);
 
   @override
-  LostAndFoundHomePageState createState()=> LostAndFoundHomePageState();
+  LostAndFoundHomePageState createState() => LostAndFoundHomePageState();
 }
 
 class LostAndFoundHomePageState extends State<LostAndFoundHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -27,98 +23,106 @@ class LostAndFoundHomePageState extends State<LostAndFoundHomePage> {
           top: false,
           bottom: false,
           child: Scaffold(
-            appBar: LostAndFoundAppBar(
-              height: 100,
-              leading: Padding(
-                padding: EdgeInsetsDirectional.only(start: 8, bottom: 8),
-                child: WButton(
-                  child: WpyPic(
-                    'assets/svg_pics/laf_butt_icons/back.svg',
-                    width: 28.w,
-                    height: 28.w,
+              appBar: LostAndFoundAppBar(
+                height: 100,
+                leading: Padding(
+                  padding: EdgeInsetsDirectional.only(start: 8, bottom: 8),
+                  child: WButton(
+                    child: WpyPic(
+                      'assets/svg_pics/laf_butt_icons/back.svg',
+                      width: 28.w,
+                      height: 28.w,
+                    ),
+                    onPressed: () {},
+
+                    ///to do
                   ),
-                  onPressed: (){}, ///to do
                 ),
-              ),
-              action: Padding(
-                padding: EdgeInsetsDirectional.only(end: 20, bottom: 12),
-                child: WButton(
-                  child: WpyPic(
-                    'assets/svg_pics/laf_butt_icons/ph_cube-bold.svg',
-                    width: 20.w,
-                    height: 20.w,
+                action: Padding(
+                  padding: EdgeInsetsDirectional.only(end: 20, bottom: 12),
+                  child: WButton(
+                    child: WpyPic(
+                      'assets/svg_pics/laf_butt_icons/ph_cube-bold.svg',
+                      width: 20.w,
+                      height: 20.w,
+                    ),
+                    onPressed: () {},
+
+                    ///to do
                   ),
-                  onPressed: (){}, ///to do
                 ),
-              ),
-              title: Padding(
-                padding: EdgeInsets.only(bottom: 2.h),
-                child: TabBar(
-                  labelStyle: TextUtil.base.bold.sp(20).whiteO60,
-                  unselectedLabelStyle: TextUtil.base.normal.sp(20).whiteO60,
-                  labelPadding: EdgeInsetsDirectional.only(start: 55.w,end: 55.w),
-                  isScrollable: true,
-                  indicatorSize: TabBarIndicatorSize.label,
-                  indicatorColor: Colors.white,
-                  indicatorWeight: 2.5.h,
-                  tabs: [
-                    Text('寻物'),
-                    Text('寻主'),
-                  ],
-                ),
-              ),
-            ),
-            body: Stack(
-              children: [
-                Container(
-                  child: TabBarView(
-                    children: [
-                      LostAndFoundSubPage(type: '寻物启事'),
-                      LostAndFoundSubPage(type: '失物招领')
+                title: Padding(
+                  padding: EdgeInsets.only(bottom: 2.h),
+                  child: TabBar(
+                    labelStyle: TextUtil.base.bold.sp(20).whiteO60,
+                    unselectedLabelStyle: TextUtil.base.normal.sp(20).whiteO60,
+                    labelPadding:
+                        EdgeInsetsDirectional.only(start: 55.w, end: 55.w),
+                    isScrollable: true,
+                    indicatorSize: TabBarIndicatorSize.label,
+                    indicatorColor: Colors.white,
+                    indicatorWeight: 2.5.h,
+                    tabs: [
+                      Text('寻物'),
+                      Text('寻主'),
                     ],
                   ),
                 ),
-                Positioned(
-                  bottom: ScreenUtil().bottomBarHeight + 90.h,
-                  right: 20.w,
-                  child: Hero(
-                    tag: "addNewPost",
-                    child: InkWell(
-                        splashColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        child: Container(
-                          height: 72.r,
-                          width: 72.r,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage("assets/images/add_post.png"),
-                            ),
-                          ),
-                        ),
-                        onTap: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (BuildContext context) {
-                                return LostAndFoundPostPage();
-                              }));
-                         }
+              ),
+              body: Stack(
+                children: [
+                  Container(
+                    child: TabBarView(
+                      children: [
+                        LostAndFoundSubPage(type: '寻物启事'),
+                        LostAndFoundSubPage(type: '失物招领')
+                      ],
                     ),
                   ),
-                )
-              ],
-            )
-          ),
-        )
-    );
+                  Positioned(
+                    bottom: ScreenUtil().bottomBarHeight + 90.h,
+                    right: 20.w,
+                    child: Hero(
+                      tag: "addNewPost",
+                      child: InkWell(
+                          splashColor: Colors.transparent,
+                          highlightColor: Colors.transparent,
+                          child: Container(
+                            height: 72.r,
+                            width: 72.r,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage("assets/images/add_post.png"),
+                              ),
+                            ),
+                          ),
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (BuildContext context) {
+                              return LostAndFoundPostPage();
+                            }));
+                          }),
+                    ),
+                  )
+                ],
+              )),
+        ));
   }
 }
 
-class LostAndFoundAppBar extends StatelessWidget implements PreferredSizeWidget {
-
+class LostAndFoundAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
   final Widget leading;
   final Widget action;
   final Widget title;
   final double height;
-  LostAndFoundAppBar({Key? key, required this.leading, required this.action, required this.title, required this.height}) : super(key: key);
+  LostAndFoundAppBar(
+      {Key? key,
+      required this.leading,
+      required this.action,
+      required this.title,
+      required this.height})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -128,8 +132,7 @@ class LostAndFoundAppBar extends StatelessWidget implements PreferredSizeWidget 
           gradient: LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
-              colors: [ColorUtil.blue2CColor, ColorUtil.blue64Color]
-          ),
+              colors: [ColorUtil.blue2CColor, ColorUtil.blue64Color]),
         ),
         child: Stack(
           children: [
@@ -142,16 +145,11 @@ class LostAndFoundAppBar extends StatelessWidget implements PreferredSizeWidget 
               alignment: AlignmentDirectional.bottomCenter,
               child: title,
             ),
-            Positioned(
-                right: 0.w,
-                bottom: 0.h,
-                child: action
-            )
+            Positioned(right: 0.w, bottom: 0.h, child: action)
           ],
-        )
-    );
+        ));
   }
 
   @override
-  Size get preferredSize => Size(ScreenUtil().screenWidth, 60.h );
+  Size get preferredSize => Size(ScreenUtil().screenWidth, 60.h);
 }
