55ae1e66b950a80d4ba51dc7030cba93ac22c603
2018X5zzt <1154322610@qq.com>
2023-11-26 17:36:15 +0800

    [apk] 4.3.18版本打包配置2(移除了失物招领的入口)

--- lib/home/home_router.dart
+++ lib/home/home_router.dart
@@ -14,7 +14,7 @@ class HomeRouter {
   static String mapCalenderPage = 'home/mapCalenderPage';
   static String restartGame = 'home/restartGame';
   static String notice = 'home/notice';
-  static String laf = 'home/laf';
+  static String laf = ''; //'home/laf' 现在还不能上线
   static final Map<String, Widget Function(dynamic arguments)> routers = {
     home: (args) => HomePage(args),
     wiki: (_) => WikiPage(),
