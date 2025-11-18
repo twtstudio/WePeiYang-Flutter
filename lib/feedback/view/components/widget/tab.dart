fabdf2c9ed0437bf7304723b8b4a72d3b52b439f
luuminhkhue <allenren114@gmail.com>
2025-11-18 11:42:54 +0800

    [fix]:优化展开窗口

--- lib/feedback/view/components/widget/tab.dart
+++ lib/feedback/view/components/widget/tab.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:we_pei_yang_flutter/commons/themes/template/wpy_theme_data.dart';
+
+import '../../../../commons/themes/wpy_theme.dart';
 
 class DaTab extends StatelessWidget {
   final String text;
+
   final bool withDropDownButton;
+
+  final bool dropdownOpen;
+
+  final bool selected;
+
+  final VoidCallback? onTap;
 
   const DaTab({
     Key? key,
     required this.text,
     required this.withDropDownButton,
+    this.dropdownOpen = false,
+    this.selected = false,
+    this.onTap,
   }) : super(key: key);
 
   @override
@@ -19,11 +32,20 @@ class DaTab extends StatelessWidget {
           children: [
             SizedBox(width: _tabPaddingWidth),
             Text(text),
-            Icon(
-              Icons.arrow_drop_down,
-              size: 20,
+            InkWell(
+              onTap: onTap, // Kích hoạt hàm mở/đóng TagsWrap
+              customBorder: const CircleBorder(), // Tạo hiệu ứng chạm hình tròn
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 4.0, vertical: 8.0),
+                child: Icon(
+                  // selected ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+                  dropdownOpen ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+                  size: 20,
+                  // color: iconColor,
+                ),
+              ),
             ),
-            if (_tabPaddingWidth > 10) SizedBox(width: _tabPaddingWidth - 10)
           ],
         ),
       );
